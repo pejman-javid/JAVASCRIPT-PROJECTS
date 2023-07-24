f1708898aceaa1117530cfa260d8c7e0d8e3c64d
pejman-javid <pejvid@gmail.com>
2023-07-24 21:13:31 +0430

    feat/pejman-javid/added new project

---
+++ one.js/index.js
@@ -0,0 +1,7 @@
+//PEJVID
+let number1 = +prompt('ENTER YOUR FIRST NUMBER')
+let number2 = +prompt("ENTER YOUR SECONED NUMBER")
+
+let sum = number1 + number2
+
+document.write("THE SUM OF YOUR TWO NUMBERS IS"  , "   ",sum,)
