7277dd4fd86cc339eb0b35eb8c902e05cfe28379
pejman-javid <pejvid@gmail.com>
2023-07-25 16:34:59 +0430

    finish/pejman-javid/added a calculator project

--- one.js/index.js
+++
@@ -1,7 +0,0 @@
-//PEJVID
-let number1 = +prompt('ENTER YOUR FIRST NUMBER')
-let number2 = +prompt("ENTER YOUR SECONED NUMBER")
-
-let sum = number1 + number2
-
-document.write("THE SUM OF YOUR TWO NUMBERS IS"  , "   ",sum,)
